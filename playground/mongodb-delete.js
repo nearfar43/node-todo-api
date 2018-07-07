b081852c275c4af8773ae234bd2e9d59f42aa9c3
nearfar43 <nearfar@rams.colostate.edu>
2018-07-06 18:10:07 -0600

    Add delete scripts

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    //delete many objects
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    //delete one object
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+    
+
+    //find one and delete 
+    
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+    // 
+    
+    // db.collection('Users').findOneAndDelete({_id: 123}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndDelete({_id: new ObjectID("5b1f552bbf747e2dc0c7cd38")}).then((result) => {
+        console.log(JSON.stringify(result, undefined, 2));
+    });
+
+    //db.close();
+});
+
