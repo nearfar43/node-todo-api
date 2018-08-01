387005b5c2b7b23cd61bc50af0da6cef012fd9cd
nearfar43 <nearfar@rams.colostate.edu>
2018-08-01 11:28:30 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,22 @@ const request = require('supertest');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
+const todos = [{
+	text: 'First'
+}, {
+	text: 'Second'
+}];
+
 
 beforeEach((done) => {
 	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => {
 		done();
 	});
 });
 
+//POST test
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
 		var text = 'Test todo text';
@@ -27,7 +36,7 @@ describe('POST /todos', () => {
 				return done(err);
 			}
 
-			Todo.find().then((todos) => {
+			Todo.find({text}).then((todos) => {
 				expect(todos.length).toBe(1);
 				expect(todos[0].text).toBe(text);
 				done();
@@ -47,9 +56,22 @@ describe('POST /todos', () => {
 			}
 
 			Todo.find().then((todos) => {
-				expect(todos.length).toBe(0);
+				expect(todos.length).toBe(2);
 				done();
 			}).catch((e) => done(e));
 		});
+	});
+});
+
+//GET test
+describe('GET /todos', () => {
+	it('should get all the todos', (done) => {
+		request(app)
+		.get('/todos')
+		.expect(200)
+		.expect((res) => {
+			expect(res.body.todos.length).toBe(2);
+		})
+		.end(done);
 	});
 });
